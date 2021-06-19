20788894152e6c1d375a73f2e1c37a3028963daa
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-06-19 15:18:28 +0500

    initial Commit

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      backgroundColor: 'black',
+      flex: 1,
+      flexDirection: "row", 
+      justifyContent: "center", // primary axix > Horizontal axix
+      alignItems: 'center', // secondary axix => Vertical axix
+    }}>
+      <View style={{
+        backgroundColor: 'red',
+        width: 100,
+        height: 100,
+      }}/>
+
+      <View style={{
+        backgroundColor: 'white',
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        top: 20,
+        left: 20
+      }}/>
+
+      <View style={{
+        backgroundColor: 'green',
+        width: 100,
+        height: 100
+      }}/>
+
     </View>
   );
 }
@@ -15,7 +39,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
