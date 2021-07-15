2f1a1fd5776aff8e78b6c11df057e6fbfc725e89
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-07-15 13:19:09 +0500

    Designed the card

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { View, Image } from 'react-native';
+import Card from './app/components/Card';
 import WelcomeScreen from './app/Screens/WelcomeScreen';
 
 export default function App() {
-  return <WelcomeScreen/>
+  return (
+    <View style={{
+      backgroundColor: '#f8f4f4',
+      padding: 20,
+      paddingTop: 100
+    }}>
+
+      <Card 
+        title='red jacket for sale' 
+        subTitle='$100' 
+        image={require('./app/assets/jacket.jpg')}/>
+    </View>
+  );
 }
