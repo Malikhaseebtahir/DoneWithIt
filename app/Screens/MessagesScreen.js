791bbce4711f3597f3b9ee3f3455ca2696e8a06f
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-07-17 14:41:41 +0500

    Implemented the Swipable and Listview functionality

--- app/Screens/MessagesScreen.js
+++ app/Screens/MessagesScreen.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { FlatList, SafeAreaView, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
 
 import SafeViewAndroid from "../common/GlobalStyles";
 import ListItemSeperator from '../components/ListItemSeperator';
 import ListItem from './../components/ListItem';
+import ListItemActionDelete from './../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
     {
         id: 1,
         title: 'T1',
@@ -27,6 +28,15 @@ const messages = [
 ]
 
 function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages);
+    const [refreshing, setRefreshing] = useState(false);
+
+    const handleDelete = message => {
+        console.log('called');
+        const newMessages = messages.filter(m => m.id !== message.id);
+        setMessages(newMessages);
+    }
+
     return(
         <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
             <FlatList
@@ -37,10 +47,24 @@ function MessagesScreen(props) {
                         title={item.title}
                         subTitle={item.description}
                         image={item.image}
-                        onPress={() => console.log('Item clicked', item)}
+                        onPress={() => console.log()}
+                        renderRightActions={() => 
+                            <ListItemActionDelete 
+                                onPress={() => handleDelete(item)}/>}
                         />
                 }
                 ItemSeparatorComponent={ListItemSeperator}
+                refreshing={refreshing}
+                onRefresh={() => {
+                    setMessages([
+                        {
+                            id: 3,
+                            title: 'T3',
+                            description: 'D3',
+                            image: require('../assets/mosh.jpg')
+                        }
+                    ])
+                }}
             />
         </SafeAreaView>
     );
