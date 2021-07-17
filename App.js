d60edc5b674db7631612a62d0a419eac5ff81f40
Malik Haseeb <malikhaseeb1935@gmail.com>
2021-07-17 17:43:24 +0500

    Designed the icon component

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
-import MessagesScreen from './app/Screens/MessagesScreen';
+import { SafeAreaView } from 'react-native';
+import SafeViewAndroid from "./app/common/GlobalStyles";
+
+import Icon from './app/components/Icon';
 
 export default function App() {
-  return <MessagesScreen/>
+  return (
+  <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+    <Icon name="email" size={50} backgroundColor='red' iconColor='blue'/>
+  </SafeAreaView>
+  );
 }
